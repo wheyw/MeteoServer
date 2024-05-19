fed927587fac72b105913bbb16178cdddc1af9fd
DaddyCalcifer <nikita.macarov@yandex.ru>
2024-05-19 16:59:36 +0300

    lite code refactor

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -5,11 +5,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Client extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
         primaryStage.setTitle("Гидрометцентр");
         primaryStage.setScene(new Scene(root, 550, 400));
         primaryStage.show();
