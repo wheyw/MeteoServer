9e98d79d0adbdfba72114fe3f5ca239637c1d4f7
Ольга Артемова <artemovao771@gmail.com>
2024-05-30 03:08:53 +0300

    Сделан рефакторинг и codestyle

--- Server/src/SController.java
+++ Server/src/SController.java
@@ -1,8 +1,7 @@
 import ClientServers.ClServ;
-import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.URL;
 import java.util.HashMap;
@@ -10,55 +9,66 @@ import java.util.Map;
 import java.util.ResourceBundle;
 
 public class SController implements Initializable {
-    public String request;
-    public String respons;
-    public Map<String, Integer> map = new HashMap();
-    private String res;
+    private String request;
+    private String response;
+    private final Map<String, Integer> map = new HashMap<>();
+    private String result;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        startServer();
+    }
 
-        try (ServerSocket server = new ServerSocket(2654);) {
+    // Метод для старта сервера
+    private void startServer() {
+        try (ServerSocket server = new ServerSocket(2654)) {
             System.out.println("Server started");
 
-            while (true)
+            while (true) {
                 try (ClServ module = new ClServ(server)) {
-                    request = module.readerLine();
-
-                    synchronized (this) {
-                        if (request.equals("1")) {
-                            module.writeLine(res);
-                        } else {
-
-                                if (map != null && map.equals(request) ) {
-                                    respons = map.get(request).toString();
-                                    module.writeLine(request);
-                                    module.writeLine(respons);
-                                    System.out.println("" + request);
-                                    System.out.println("" + respons);
-                                    res = new String(request + respons);
-                                    System.out.println(res);
-                                } else {
-
-                                    respons = module.readerLine();
-                                    map.put(request, Integer.valueOf(respons));
-                                    module.writeLine(request);
-                                    module.writeLine(respons);
-                                    System.out.println(map);
-                                    //System.out.println("" + request);
-                                    //System.out.println("" + respons);
-                                    res = new String(request + respons);
-                                    System.out.println(res);
-                                }
-                        }
-                    }
-
+                    handleClient(module);
                 } catch (IOException e) {
-                    throw new RuntimeException(e);
+                    System.err.println("Error handling client: " + e.getMessage());
+                    e.printStackTrace();
                 }
-
+            }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.err.println("Error starting server: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    // Метод для обработки клиента
+    private void handleClient(ClServ module) throws IOException {
+        request = module.readLine();
+
+        synchronized (this) {
+            if ("1".equals(request)) {
+                module.writeLine(result);
+            } else {
+                processRequest(module);
+            }
+        }
+    }
+
+    // Метод для обработки запроса
+    private void processRequest(ClServ module) throws IOException {
+        if (map.containsKey(request)) {
+            response = map.get(request).toString();
+            module.writeLine(request);
+            module.writeLine(response);
+            System.out.println("Request: " + request);
+            System.out.println("Response: " + response);
+            result = request + response;
+            System.out.println("Result: " + result);
+        } else {
+            response = module.readLine();
+            map.put(request, Integer.valueOf(response));
+            module.writeLine(request);
+            module.writeLine(response);
+            System.out.println("Map: " + map);
+            result = request + response;
+            System.out.println("Result: " + result);
         }
     }
 }
