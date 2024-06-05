e75cc8b66eee7b15d8f291ed9409dc45892abe68
EDB <dragonoidbakugan@mail.ru>
2024-06-05 23:33:29 +0300

    feat: add historyRequests/Responses,checks and refactor code

--- Server/src/SController.java
+++ Server/src/SController.java
@@ -11,54 +11,53 @@ import java.util.ResourceBundle;
 
 public class SController implements Initializable {
     public String request;
-    public String respons;
-    public Map<String, Integer> map = new HashMap();
-    private String res;
+    public String response;
+    private final HashMap<String, Integer> map = new HashMap<>();
+    private String result;
+    private final Object lock = new Object();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        try (ServerSocket server = new ServerSocket(2654)) {
+            System.out.println("Сервер запущен");
 
-        try (ServerSocket server = new ServerSocket(2654);) {
-            System.out.println("Server started now");
-
-            while (true)
+            while (true) {
                 try (ClServ module = new ClServ(server)) {
                     request = module.readerLine();
-
-                    synchronized (this) {
-                        if (request.equals("1")) {
-                            module.writeLine(res);
-                        } else {
-
-                                if (map != null && map.equals(request) ) {
-                                    respons = map.get(request).toString();
-                                    module.writeLine(request);
-                                    module.writeLine(respons);
-                                    System.out.println("" + request);
-                                    System.out.println("" + respons);
-                                    res = new String(request + respons);
-                                    System.out.println(res);
-                                } else {
-
-                                    respons = module.readerLine();
-                                    map.put(request, Integer.valueOf(respons));
-                                    module.writeLine(request);
-                                    module.writeLine(respons);
-                                    System.out.println(map);
-                                    //System.out.println("" + request);
-                                    //System.out.println("" + respons);
-                                    res = new String(request + respons);
-                                    System.out.println(res);
-                                }
-                        }
-                    }
-
+                    result = processRequest(request, module);
+                    System.out.println(result);
                 } catch (IOException e) {
-                    throw new RuntimeException(e);
+                    System.err.println("Ошибка при обработке запроса клиента: " + e.getMessage());
                 }
-
+            }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.err.println("Ошибка при запуске сервера: " + e.getMessage());
+        }
+    }
+
+    private String processRequest(String request, ClServ module) {
+        String result = "";
+        synchronized (lock) {
+            if (request.equals("1")) {
+                module.writeLine(result);
+            } else {
+                Integer value = getMapValue(request);
+                if (value != null) {
+                    response = value.toString();
+                    result = request + " " + response;
+                } else {
+                    response = module.readerLine();
+                    map.put(request, Integer.valueOf(response));
+                    result = request + " " + response;
+                }
+            }
+        }
+        return result;
+    }
+
+    public Integer getMapValue(String key) {
+        synchronized (lock) {
+            return map.get(key);
         }
     }
 }
